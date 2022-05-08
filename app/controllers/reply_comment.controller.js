a81018052b21a188f140f9c6451dc448f5c3287a
vutran2305 <78093022+vutran2305@users.noreply.github.com>
2022-05-08 11:10:22 +0700

    uodate

--- app/controllers/reply_comment.controller.js
+++ app/controllers/reply_comment.controller.js
@@ -1,15 +1,15 @@
-const Comment = require('../services/reply_comment.service');
+const ReplyComment = require('../services/reply_comment.service');
 
 //get list
     exports.get_list_reply_comment = function(req,res){
-        Comment.get_all(function(data){
+        ReplyComment.get_all(function(data){
             res.send({result:data});
         });
         
     }
     // get detail
     exports.get_detail = function(req,res){
-        Comment.getById(req.params.id, function(respnse){
+        ReplyComment.getById(req.params.id, function(respnse){
             res.send({result:respnse});
         });
         
@@ -19,7 +19,7 @@ const Comment = require('../services/reply_comment.service');
     //cài body_parser
     exports.add_reply_comment = function (req,res){
         var data = req.body;   
-        Comment.create(data,function(respnse){
+        ReplyComment.create(data,function(respnse){
             res.send({result:respnse});
         });
     } 
@@ -27,14 +27,14 @@ const Comment = require('../services/reply_comment.service');
     exports.delete_reply_comment = function(req,res){
     var id = req.params.id;
     //callback
-    Comment.remove(id,function(respnse){
+    ReplyComment.remove(id,function(respnse){
         res.send({result:respnse});
     });
     }
     //put (update gioongs add can data)
     exports.update_reply_comment = function(req,res){
     var data = req.body;   
-    Comment.update(data,function(respnse){
+    ReplyComment.update(data,function(respnse){
         res.send({result:respnse});
     });
     }
--- index.js
+++ index.js
@@ -1,27 +1,67 @@
 var express = require ('express');
 var app = express() ;
+const session = require('express-session');
+const mysql = require('mysql');
 
+const connection =mysql.createConnection({
+    host:'localhost',
+    user: 'root',
+    password:'',
+    database: 'news1'
+});
 
 //cấu hình body-parser
 var body_parser = require('body-parser');
 app.use(body_parser.urlencoded({extend:false}));
 app.use(body_parser.json());
+app.use(session({
+	secret: 'secret',
+	resave: true,
+	saveUninitialized: true
+}));
 
 require('./app/routes/news.router')(app);
 require('./app/routes/topic.router')(app);
 require('./app/routes/comment.router')(app);
-require('./app/routes/reply_comment')(app);
+require('./app/routes/reply_comment.router')(app);
+require('./app/routes/register.router')(app);
 
-app.post('/login',function(req,res){
-    if(req.body.username=='vutran2305'&& req.body.password=='12345'){
-        res.json('đăng nhập thành công');
-    }
-    else{
-        res.send('Đăng nhập thất bại'); 
-    }
+// app.post('/login',function(req,res){
+//     if(req.body.username=='vutran2305'&& req.body.password=='12345'){
+//         res.json('đăng nhập thành công');
+//     }
+//     else{
+//         res.send('Đăng nhập thất bại'); 
+//     }
+// });
+app.post('/login', function(request, response) {
+	// Capture the input fields
+	let username = request.body.username;
+	let password = request.body.password;
+	// Ensure the input fields exists and are not empty
+	if (username && password) {
+		// Execute SQL query that'll select the account from the database based on the specified username and password
+		connection.query('SELECT * FROM user WHERE UserName = ? AND Password = ?', [username, password], function(error, results, fields) {
+			// If there is an issue with the query, output the error
+			if (error) throw error;
+			// If the account exists
+			if (results.length > 0) {
+				// Authenticate the user
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.send('Đăng nhập thành công');
+			} else {
+				response.send('Username or Password không đúng !');
+			}			
+			response.end();
+		});
+	} else {
+		response.send('Vui lòng nhập Username and Password!!!');
+		response.end();
+	}
 });
 
 //listen on port 3000 
 app.listen(4000,function () {
     console.log("Server listenning on http://localhost:4000");
-})
+}) 
