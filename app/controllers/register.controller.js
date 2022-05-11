e0c60850a27638d4c0041a015b478c4ed1c9468e
TVV <van.tv@techchain.vn>
2022-05-11 18:54:33 +0700

    update code register

--- app/controllers/register.controller.js
+++ app/controllers/register.controller.js
@@ -6,10 +6,7 @@ const Register = require("../services/register.service");
 exports.add_user = function (req, res) {
   console.log("check req body: ", req);
     var data = req.body;
-  // var data = req.query;
-  // console.log("Check data: ", data);
   Register.create(data, function (respnse) {
-    console.log("check data :", data);
     res.send({ result: respnse });
   });
 };
