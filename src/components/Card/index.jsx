29865197e6ef671cea927917f8a81719caa83d30
TVV <van.tv@techchain.vn>
2022-05-18 22:46:03 +0700

    update card ui topic

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,35 +1,41 @@
 import "./card.css";
 import moment from "moment";
 import { Link } from "react-router-dom";
+import { useHistory } from "react-router-dom";
 const CardTopic = ({ item }) => {
-  const {
-    News_Id,
-    News_Tiltle,
-    News_description,
-    News_Thumbnail,
-    Created_At,
-  } = item;
-  const { data, type } = News_Thumbnail;
+  const history = useHistory();
+  const { News_Id, News_Tiltle, News_description, News_Thumbnail, Created_At } =
+    item;
+  const { data } = News_Thumbnail;
   const base64String = btoa(String.fromCharCode(...new Uint8Array(data)));
   moment.locale("vi");
   moment().format("LL");
   return (
     <>
-      <div className="card-container">
+      <div
+        className="card-container"
+        onClick={() => history.push(`/post/${News_Id}`)}
+      >
         <div className="card-image">
-        <Link to={`/post/${News_Id}`}>
-          {" "}
-          <img
-            src={`data:image/png;base64,${base64String}`}
-            alt={News_Tiltle}
-            width="100%"
-          />
-        </Link>
+          <Link to={`/post/${News_Id}`}>
+            {" "}
+            <img
+              src={`data:image/png;base64,${base64String}`}
+              alt={News_Tiltle}
+              width="100%"
+            />
+          </Link>
         </div>
         <div className="card-meta">
-          <div className="card-title">{News_Tiltle}</div>
-          <span className="card-date">{`${moment(`${Created_At}`).format("LL")}`}</span>
-          <div className="card-description">{News_description}</div>
+          <div className="card-title">
+            <Link to={`/post/${News_Id}`}>{News_Tiltle}</Link>
+          </div>
+          <div className="card-date">
+            <span>{`${moment(`${Created_At}`).format("LL")}`}</span>
+          </div>
+          <div className="card-description">
+            <span>{News_description}</span>
+          </div>
         </div>
       </div>
     </>
