e6a410e8abe163755921ed2a4ff7dc81e7b321d0
vutran2305 <78093022+vutran2305@users.noreply.github.com>
2022-05-22 17:38:48 +0700

    update

--- app/controllers/news.controller.js
+++ app/controllers/news.controller.js
@@ -10,6 +10,13 @@ const PAGE_SIZE =4;
         });
         
     }
+    //get list top
+    exports.getListTop = function(req,res){
+        News.get_top(function(data){
+            res.send({result:data});
+        });
+        
+    }
     exports.get_list_news_by_create_at = function(req,res){
         var page =parseInt(req.query.page )|| 1;
         var star = (page -1)* PAGE_SIZE;
@@ -29,7 +36,7 @@ const PAGE_SIZE =4;
         var page =parseInt(req.query.page )|| 1;
         var star = (page -1)* PAGE_SIZE;
         var end = page * PAGE_SIZE;
-        News.get_by_newsTitle(req.body.News_Tiltle,function(respnse,total){
+        News.get_by_newsTitle(req.body.News_Title,function(respnse,total){
             if (respnse != null) {
                 res.send({result:respnse.slice(star,end),total});
             }
