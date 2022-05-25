e3cd3ba2d356c20b5c68f7cb65f2e8e085d2c6b5
TVV <van.tv@techchain.vn>
2022-05-25 21:16:27 +0700

    update single page

--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -7,23 +7,18 @@ import { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
 import { fetchDetailRequest } from "../../store/action/DetailAction";
+import { fetchListRequest } from "../../store/action/NewsAction";
+import CardTopic from "../../components/Card";
 import axios from "axios";
 import moment from "moment";
 import "moment/locale/vi";
 import "./single.css";
+import { result } from "lodash";
 const Single = () => {
   moment.locale("vi");
   moment().format("LL");
   const { id } = useParams();
   const [speech, setSpeech] = useState();
-  /*
-  tk account 1: tvv2k, pass: tvvDUE44k14@ {done pay}
-   api-key: pf3R8KUQA38QoRuhApBVlBQsZZi6EUqy
-   tk account 2: tvvdue4414, pass: tương tự
-   "api-key": "8ZZ39H0kTOWmF56YMGkTMIHpW7GsbG7j",
-   tk account 3: tvvdue, pass: tvvDUE44k14@
-   apikey: "kK5WLZn5nddU3sgz0jjEH2KD2HbCmudY"
-   */
   const url = "https://api.fpt.ai/hmi/tts/v5";
   let config = {
     headers: {
@@ -41,12 +36,10 @@ const Single = () => {
     detailContent;
   let NewArr = "";
   const fetchAudio = async (NewArr) => {
-    const result =
-    await axios
+    const result = await axios
       .post(`${url}`, NewArr, config)
       .then((res) => {
         return res?.data?.async;
-        // setSpeech(res?.data?.async);
       })
       .catch((err) => {
         console.log("AXIOS ERROR: ", err);
@@ -70,25 +63,21 @@ const Single = () => {
     console.log("Check newArr:", NewArr);
     fetchAudio(NewArr);
   }, [News_Content, NewArr, speech]);
-  {
-    console.log("check speech:", speech);
-  }
+
   return (
     <>
       <div className="single">
-        <h2>{News_Title}</h2>
-        <audio controls>
-          {speech && <source src={speech} type="audio/mpeg" />}
-        </audio>
-        <p className="date" style={{ marginTop: "25px" }}>{`${moment(
-          `${Created_At}`
-        ).format("LL")}`}</p>
-        <p className="description">{News_description}</p>
-        <div dangerouslySetInnerHTML={{ __html: News_Content }} />
-        <Comments
-          commentsUrl={`http://localhost:3000/${id}`}
-          currentUserId="1"
-        />
+        <div className="single-content">
+          <h2>{News_Title}</h2>
+          <audio controls>
+            {speech && <source src={speech} type="audio/mpeg" />}
+          </audio>
+          <p className="date" style={{ marginTop: "25px" }}>{`${moment(
+            `${Created_At}`
+          ).format("LL")}`}</p>
+          <p className="description">{News_description}</p>
+          <div dangerouslySetInnerHTML={{ __html: News_Content }} />
+        </div>
       </div>
     </>
   );
