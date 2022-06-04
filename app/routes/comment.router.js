bf9ba88efe08e60243b80f677a8d491c757172f4
TVV <van.tv@techchain.vn>
2022-06-04 08:50:38 +0700

    remove useless folder

--- app/routes/comment.router.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (router){
-    var commnetController = require ('../controllers/comment.controller');
-
-router.get('/comment/list/:id',commnetController.get_list_comment);
-router.get('/comment/user/:id',commnetController.get_detail);
-router.post('/comment/add',commnetController.add_comment);
-router.delete('/comment/delete/:id',commnetController.delete_comment);
-router.put('/comment/update/:id',commnetController.update_comment);
-} 
--- app/routes/reply_comment.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (router){
-    var reply_commnetController = require ('../controllers/reply_comment.controller');
-
-router.get('/reply_comment/list',reply_commnetController.get_list_reply_comment);
-router.get('/reply_comment/detail/:id',reply_commnetController.get_detail);
-router.post('/reply_comment/add',reply_commnetController.add_reply_comment);
-router.delete('/reply_comment/delete/:id',reply_commnetController.delete_reply_comment);
-router.put('/reply_comment/update',reply_commnetController.update_reply_comment);
-} 
