4fad935f52a3581decdf99649e680a27254aa02e
TVV <van.tv@techchain.vn>
2022-06-09 22:50:41 +0700

    updaete

--- app/controllers/register.controller.js
+++ app/controllers/register.controller.js
@@ -5,32 +5,34 @@ const Register = require("../services/register.service");
 //cài body_parser
 exports.add_user = function (req, res) {
   console.log("check req body: ", req);
-    var data = req.body;
+  var data = req.body;
+
   Register.create(data, function (respnse) {
-    res.send({ result: respnse });
+    if (data === {}) {
+      res.send({ result: "Vui lòng nhập dữ liệu" });
+    } else res.send({ result: respnse });
   });
 };
-exports.get_list = function(req,res){
+exports.get_list = function (req, res) {
   // var page =parseInt(req.query.page )|| 1;
   // var star = (page -1)* PAGE_SIZE;
   // var end = page * PAGE_SIZE;
-  Register.get_all(function(data , total){
-      res.send({result:data ,total});
+  Register.get_all(function (data, total) {
+    res.send({ result: data, total });
   });
-  
 };
-exports.update_register = function(req,res){
-  var data = req.body;   
-  Register.update(data,function(respnse){
-      res.send({result:respnse});
+exports.update_register = function (req, res) {
+  var data = req.body;
+  Register.update(data, function (respnse) {
+    res.send({ result: respnse });
   });
-}
+};
 
 //delete
-exports.delete_register = function(req,res){
+exports.delete_register = function (req, res) {
   var id = req.params.id;
   //callback
-  Register.remove(id,function(respnse){
-      res.send({result:respnse});
+  Register.remove(id, function (respnse) {
+    res.send({ result: respnse });
   });
-  }
+};
